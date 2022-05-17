d7cd6ce09fed07a9e3c5a6a93a16eb57a55fc2bf
Piotr Pawełczak <pecio@student.agh.edu.pl>
2022-05-17 21:04:30 +0200

    Create dataset

--- load_data.py
+++ load_data.py
@@ -4,9 +4,11 @@ import os
 
 DATASET_PATH = 'data'
 
-def get_spectrogram(file_path, n_fft, hop_length):
+def get_spectrogram(file_path, n_fft, hop_length, expected_signal_shape):
 
     signal, sample_rate = librosa.load(file_path)
+    signal = signal[:expected_signal_shape]
+
     stft = librosa.stft(signal, n_fft=n_fft, hop_length=hop_length)
     spectrogram = librosa.amplitude_to_db(np.abs(stft))
 
@@ -14,9 +16,11 @@ def get_spectrogram(file_path, n_fft, hop_length):
 
 
 
-def get_mfcc(file_path, n_fft, hop_length, n_mfcc):
+def get_mfcc(file_path, n_fft, hop_length, n_mfcc, expected_signal_shape):
     
     signal, sample_rate = librosa.load(file_path)
+    signal = signal[:expected_signal_shape]
+
     mfcc = librosa.feature.mfcc(y=signal, sr=sample_rate, hop_length=hop_length, n_fft=n_fft, n_mfcc=n_mfcc)
 
     return mfcc
